d148fb52ca8b2f76604b67f6051786f7d31f2558
Artem Shukatka <artem.shukatka@gmail.com>
2023-01-15 20:36:27 +0200

    for mentor 1

--- js/task-08.js
+++ js/task-08.js
@@ -3,9 +3,7 @@ formEl.addEventListener("submit", (event) => {
   event.preventDefault();
   const elementsOfForm = event.currentTarget.elements;
   let email = elementsOfForm.email.value;
-  console.log(email);
   let password = elementsOfForm.password.value;
-  console.log(password);
   if (email === null || password === null || email === "" || password === "") {
     alert("All the inputs must be fullfilled");
   } else {
