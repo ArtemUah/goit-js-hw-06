6c6e3e9c5d8a1276f15e0a4f3cc65edc82cf8535
Artem Shukatka <artem.shukatka@gmail.com>
2023-01-15 20:41:16 +0200

    for mentor 2

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,5 @@
 const inputEl = document.getElementById("font-size-control");
 const valueOfInputEl = inputEl.value;
-console.log(typeof valueOfInputEl);
-console.log(valueOfInputEl);
 inputEl.addEventListener("input", (event) => {
   const textEl = document.getElementById("text");
   textEl.style.fontSize = `${inputEl.value}px`;
